507e6d09435b66d7188a9b4c96dea9ed5e3d1e87
alexandros filotheou <alefil@kth.se>
2016-02-26 00:53:42 +0100

    Implements the hybrid controller
    	modified:   CFiles/OwnCode/Controller.c
    	modified:   CFiles/OwnCode/OwnVariables.c
    	modified:   CFiles/OwnCode/RenewControllerState.c

--- Simulation Environment/CFiles/OwnCode/Controller.c
+++ Simulation Environment/CFiles/OwnCode/Controller.c
@@ -1,27 +1,27 @@
-// Calculate theta_R, the angle between the robot and the goal
-if (fabs(x - xg) > 0.00001)
-{
-  theta_R = atan2(y-yg, x-xg) * 180 / PI - 180;
+// Calculate theta_R, the angle between the line that connects the robot and the
+// goal, and the x-axis
+theta_R = atan2( yg-y0, xg-x0 ) * 180 / PI;
 
-  // Limit theta-R to (-180,180]
-  if (theta_R > 180)
-  {
-    theta_R = theta_R - 360;
-  }
-  if (theta_R <= -180)
-  {
-    theta_R = theta_R + 360;
-  }
+// Choose control strategy
+if ( fabs(theta - theta_R) > angle_tolerance )
+{
+  control_strategy = ROTATION_CONTROL;
+}
+else if ( fabs(cos(theta_R * PI / 180) * (xg - x)
+    + sin(theta_R * PI / 180) * (yg - y)) > distance_tolerance )
+{
+  control_strategy = TRANSLATION_CONTROL;
 }
 else
 {
-  theta_R = 0;
+  control_strategy = STOP_CONTROL;
 }
 
-/*control_strategy = ROTATION_CONTROL;*/
-control_strategy = TRANSLATION_CONTROL;
 
-switch (control_strategy)
+/*control_strategy = ROTATION_CONTROL;*/
+/*control_strategy = TRANSLATION_CONTROL;*/
+
+switch ( control_strategy )
 {
   case ROTATION_CONTROL:
 
@@ -31,30 +31,38 @@ switch (control_strategy)
     /*right = 0.5 * u_psi;*/
     /*left = -right;*/
 
-    u_omega = r_K_omega * (cos(theta) * (x0 - x) + sin(theta) * (y0 - y));
+    u_omega = r_K_omega * (cos(theta * PI / 180) * (x0 - x) + sin(theta * PI / 180) * (y0 - y));
 
     // Task 10
     /*left = u_omega;*/
     /*right = u_omega;*/
 
     break;
+
   case TRANSLATION_CONTROL:
 
-    u_omega = t_K_omega * (cos(theta_R) * (xg-x) + sin(theta_R) * (yg-y));
+    u_omega = t_K_omega * (cos(theta_R * PI / 180) * (xg-x)
+      + sin(theta_R * PI / 180) * (yg-y));
 
     // Task 13
-    left = u_omega;
-    right = u_omega;
+    /*left = u_omega;*/
+    /*right = u_omega;*/
 
     u_psi = t_K_psi *
-      (sin(theta_R) * (x + t_p * cos(theta) - x0)
-      - cos(theta_R) * (y + t_p * sin(theta) - y0));
+      (sin(theta_R * PI / 180) * (x + t_p * cos(theta * PI / 180) - x0)
+       - cos(theta_R * PI / 180) * (y + t_p * sin(theta * PI / 180) - y0));
 
     // Task 16
     /*right = 0.5 * u_psi;*/
     /*left = -right;*/
     break;
+
+  case STOP_CONTROL:
+
+    u_psi = 0;
+    u_omega = 0;
+    break;
 }
 
-//left = 0.5 * (2 * u_omega - u_psi);
-//right = 0.5 * (2 * u_omega + u_psi);
+left = u_omega - 0.5 * u_psi;
+right = u_omega + 0.5 * u_psi;
--- Simulation Environment/CFiles/OwnCode/OwnVariables.c
+++ Simulation Environment/CFiles/OwnCode/OwnVariables.c
@@ -4,12 +4,19 @@ double L_true = 0.5052864456892;
 // Control strategy
 #define ROTATION_CONTROL 0
 #define TRANSLATION_CONTROL 1
+#define STOP_CONTROL 2
 
-// The control strategy: rotation or translation
+// The control strategy: R(otation), T(ranslation), S(top)
 int control_strategy = 0;
 
 // The angle between the robot and the goal
 double theta_R = 0;
+
+// The robot cannot (in general) be rotated *exactly* theta_R degrees
+double angle_tolerance = 6;
+
+// The robot cannot (in general) be translated *exactly* to the goal (cm)
+double distance_tolerance = 20;
 
 // Sampling time. Should be obtained through the GUI?
 double Ts = 1;
@@ -21,20 +28,20 @@ double u_omega = 0;
 // Part I of Rotation Control
 double r_K_psi_min = 0;
 double r_K_psi_max = 2 * L_true / (R_true * Ts);
-double r_K_psi = L_true / (R_true * Ts);
+double r_K_psi = r_K_psi_max * 0.5;
 
 // Part II of Rotation Control
 double r_K_omega_min = 0;
 double r_K_omega_max = 2 / (R_true * Ts);
-double r_K_omega = 1 / (R_true * Ts);
+double r_K_omega = r_K_omega_max * 0.5;
 
 // Part II of Line Following Control
-double t_p = 1;
+double t_p = 40;
 double t_K_psi_min = -2 * L_true / (t_p * Ts * R_true);
 double t_K_psi_max = 0;
-double t_K_psi = -L_true / (t_p * Ts * R_true);
+double t_K_psi = t_K_psi_min * 0.5;
 
 // Part I of Line Following Control
 double t_K_omega_min = 0;
 double t_K_omega_max = 2 / (R_true * Ts);
-double t_K_omega = 1 / (R_true * Ts);
+double t_K_omega = t_K_omega_max * 0.4;
