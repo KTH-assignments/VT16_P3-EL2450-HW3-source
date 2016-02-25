7c4e29754fbff5c516dc1c078c63fe7650ab72f7
rosr <rosr@kth.se>
2016-02-25 11:04:07 +0100

    Corrects translational controller's variables
    	modified:   Simulation Environment/CFiles/OwnCode/OwnVariables.c

--- Simulation Environment/CFiles/OwnCode/OwnVariables.c
+++ Simulation Environment/CFiles/OwnCode/OwnVariables.c
@@ -28,13 +28,13 @@ double r_K_omega_min = 0;
 double r_K_omega_max = 2 / (R_true * Ts);
 double r_K_omega = 1 / (R_true * Ts);
 
-// Part I of Line Following Control
-double t_K_psi_min = 0;
-double t_K_psi_max = 2 * L_true / (R_true * Ts);
-double t_K_psi = L_true / (R_true * Ts);
-
 // Part II of Line Following Control
 double t_p = 1;
-double t_K_omega_min = -2 * L_true / (t_p * Ts * R_true);
-double t_K_omega_max = 0;
-double t_K_omega = -L_true / (t_p * Ts * R_true);
+double t_K_psi_min = -2 * L_true / (t_p * Ts * R_true);
+double t_K_psi_max = 0;
+double t_K_psi = -L_true / (t_p * Ts * R_true);
+
+// Part I of Line Following Control
+double t_K_omega_min = 0;
+double t_K_omega_max = 2 / (R_true * Ts);
+double t_K_omega = 1 / (R_true * Ts);
